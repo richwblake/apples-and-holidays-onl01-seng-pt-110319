434068b11002a7cccfac0274cd65d51bd7bf115c
Wills Blake <richwblake@gmail.com>
2019-11-09 19:27:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,6 +78,10 @@ def all_supplies_in_holidays(holiday_hash)
   }  
 end
 
+def format_holiday_name ( holiday_name )
+  
+end
+
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
